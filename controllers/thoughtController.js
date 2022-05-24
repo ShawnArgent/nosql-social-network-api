860d11a17f0a8f56f8ca3ac4556157f0f5812c0b
Shawn Argent <kshawn001@gmail.com>
2022-05-24 09:59:15 -0700

    changes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,29 +2,30 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
   getThoughts(req, res) {
-    Thought.find({ _id: params.id })
+    Thought.find({})
+      .populate({ path: 'reactions', select: '-__v' })
       .select('-__v')
-      .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => res.status(500).json(err));
   },
   // Get single thought
   getThoughtById({ params }, res) {
     Thought.findOne({ _id: params.id })
+      .populate({ path: 'reactions', select: '-__v' })
       .select('-__v')
-      .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => res.status(500).json(err));
   },
   // Create single thought
-  addThought({ params, body }, res) {
+  addThought({ body }, res) {
     Thought.create(body)
       .then(({ _id }) => {
-        return User.findOneAndUpdate({ _id: params.id }, body, { $push: { thoughts: _id } }, { new: true });
+        User.findOneAndUpdate({ _id: body.user.id }, body, { $push: { thoughts: dbThoughtData._id } }, { new: true });
       })
       .then((dbUserData) => {
         if (!dbUserData) {
-          return res.status(404).json({ message: 'Not found.' });
+          res.status(404).json({ message: 'Not found.' });
+          return;
         }
         res.json(dbUserData);
       })
@@ -50,7 +51,7 @@ const thoughtController = {
           res.status(404).json({ message: 'Thought not found.' });
           return;
         }
-        res.status(200).json(dbThoughtData);
+        res.status.json(dbThoughtData);
       })
       .catch((err) => res.status(500).json(err));
   },
